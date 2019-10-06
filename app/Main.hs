65e842852a0fbbdff467e25513a9403825b0a039
loveppears <litar929@gmail.com>
2019-10-06 22:46:04 +0900

    add sample:

--- app/Main.hs
+++ app/Main.hs
@@ -3,5 +3,14 @@ module Main where
 import Lib
 import Coroutine
 
+-- printOne n = do
+--   liftIO (print n)
+--   yield
+
+-- example = runCoroutineT $ do
+--   fork $ replicateM_ 3 (printOne 3)
+--   fork $ replicateM_ 4 (printOne 4)
+--   replicateM_ 2 (printOne 2)
+
 main :: IO ()
 main = someFunc
